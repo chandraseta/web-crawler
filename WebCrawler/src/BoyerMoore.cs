faa473146f2a16b3f32092bf3f2ddb319bb979ca
Rionaldi Chandraseta <rionaldi.chandraseta@gmail.com>
2017-04-22 02:56:00 +0700

    Added KMP and BoyerMoore algorithm

---
+++ WebCrawler/src/BoyerMoore.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.src
+{
+    public class BoyerMoore
+    {
+        private string pattern;
+        private int[] last_occurence;
+
+        public BoyerMoore(string keyword)
+        {
+            pattern = String.Copy(keyword);
+            last_occurence = new int[pattern.Length];
+            last_occurence = CreateLastOccurence();
+        }
+
+        public int[] CreateLastOccurence()
+        {
+            int[] res = new int[128];
+            for (int i=0; i<128; i++)
+            {
+                res[i] = -1;
+            }
+            for (int i=0; i<pattern.Length; i++)
+            {
+                res[pattern[i]] = i;
+            }
+            return res;
+        }
+
+        public int Search(string text)
+        {
+            int text_length = text.Length;
+            int pattern_length = pattern.Length;
+            int text_idx = pattern_length - 1;
+
+            if (text_idx > text_length-1)
+            {
+                return -1;
+            }
+
+            int pattern_idx = pattern.Length - 1;
+            do
+            {
+                if (pattern[pattern_idx] == text[text_idx])
+                {
+                    if (pattern_idx == 0)
+                    {
+                        return text_idx;
+                    }
+                    else
+                    {
+                        text_idx--;
+                        pattern_idx--;
+                    }
+                }
+                else
+                {
+                    int last_occured = last_occurence[text[text_idx]];
+                    text_idx = text_idx + pattern_length - Math.Min(pattern_idx, last_occured + 1);
+                    pattern_idx = pattern_length - 1;
+                }
+            } while (text_idx <= text_length - 1);
+
+            return -1;
+        }
+    }
+}
---
+++ WebCrawler/src/KMP.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.src
+{
+    public class KMP
+    {
+        private string pattern;
+        private int[] border;
+
+        public KMP(string keyword)
+        {
+            pattern = String.Copy(keyword);
+            border = new int[pattern.Length];
+            border = CreateBorder();
+        }
+
+        public int[] CreateBorder()
+        {
+            int[] res = new int[pattern.Length];
+            res[0] = 0;
+            
+            int len = pattern.Length;
+            int j = 0;
+            int idx = 1;
+
+            while (idx < len)
+            {
+                if (pattern[j] == pattern[idx])
+                {
+                    res[idx] = j + 1;
+                    idx++;
+                    j++;
+                }
+                else if (j > 0)
+                {
+                    j = res[j - 1];
+                }
+                else
+                {
+                    res[idx] = 0;
+                    idx++;
+                }
+            }
+
+            return res;
+        }
+
+        public int Search(string text)
+        {
+            int text_length = text.Length;
+            int pattern_length = pattern.Length;
+
+            int text_idx = 0;
+            int pattern_idx = 0;
+
+            while (text_idx < text_length)
+            {
+                if (pattern[pattern_idx] == text[text_idx])
+                {
+                    if (pattern_idx == pattern_length-1)
+                    {
+                        return text_idx - pattern_length + 1;
+                    }
+                    text_idx++;
+                    pattern_idx++;
+                }
+                else if (pattern_idx > 0)
+                {
+                    j = border[j - 1];
+                }
+                else
+                {
+                    text_idx++;
+                }
+            }
+            return -1;
+        }
+    }
+}
